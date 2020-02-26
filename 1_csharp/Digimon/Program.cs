8280837ab7a16ae0a314ffdfa509dc9ac8139347
jeremy <jeremychheav@yahoo.com>
2020-02-26 11:27:09 -0600

    add 1_csharp directory

---
+++ 1_csharp/Digimon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Digimon
+{
+    internal class Program
+    {
+        internal class Digimon {}
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Fight(null, null);
+        }
+
+        public void Fight(Digimon a, Digimon b) 
+        {
+            System.Console.WriteLine("{0} fights {1}", a, b);
+        }
+    }
+}
